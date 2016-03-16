51b40e416143eb641db1c5ea3f911bd925bb0ead
Tom Pereira <tompe@asos.com>
2016-03-16 19:18:16 +0000

    Converted product detail into modal overlay

---
+++ Asos.FashionEmergency.Web/scripts/view-product.js
@@ -0,0 +1,15 @@
+﻿$('.product a').on('click', function(e) {
+    $.ajax(e.currentTarget.href, {
+        type: 'GET'
+    }).done(function(markup) {
+        $('.product-info .content')[0].innerHTML = markup;
+        $('#overlay').css('display', 'block');
+        $('.product-info').css('display', 'block');
+    });
+    e.preventDefault();
+});
+
+$('.modal-close').on('click', function() {
+    $('#overlay').css('display', 'none');
+    $('.product-info').css('display', 'none');
+});
