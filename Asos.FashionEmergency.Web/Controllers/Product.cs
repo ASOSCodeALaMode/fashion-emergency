6e6145a19e68b3dbe1c4be03ce9b278128c8287b
Rob Bell <me@rob-bell.net>
2016-03-16 19:41:33 +0000

    Push dummy products out to repository

--- Asos.FashionEmergency.Web/Controllers/Product.cs
+++ Asos.FashionEmergency.Web/Controllers/Product.cs
@@ -8,5 +8,6 @@
         public decimal Price { get; set; }
         public int Availability { get; set; }
         public string ImageUrl { get; set; }
+        public int StoreId { get; set; }
     }
 }
