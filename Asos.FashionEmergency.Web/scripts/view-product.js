a70f48ac1d3cc41edaed87b9b1f83511ee4916d7
Tom Pereira <tompe@asos.com>
2016-03-16 19:28:00 +0000

    ensured scroll removed when viewing overlay

--- Asos.FashionEmergency.Web/scripts/view-product.js
+++ Asos.FashionEmergency.Web/scripts/view-product.js
@@ -1,10 +1,14 @@
-﻿$('.product a').on('click', function(e) {
+﻿var scrollPositionToRestore;
+
+$('.product a').on('click', function(e) {
     $.ajax(e.currentTarget.href, {
         type: 'GET'
     }).done(function(markup) {
         $('.product-info .content')[0].innerHTML = markup;
         $('#overlay').css('display', 'block');
         $('.product-info').css('display', 'block');
+        scrollPositionToRestore = $('body').scrollTop();
+        $('body').css('overflow', 'hidden');
     });
     e.preventDefault();
 });
@@ -12,4 +16,6 @@
 $('.modal-close').on('click', function() {
     $('#overlay').css('display', 'none');
     $('.product-info').css('display', 'none');
+    $('body').css('overflow', 'scroll');
+    $('body').scrollTop(scrollPositionToRestore);
 });
