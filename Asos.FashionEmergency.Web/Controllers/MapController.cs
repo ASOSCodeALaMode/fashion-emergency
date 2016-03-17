9d10c780fdf8eae7024f01a71f5e5d27e8d493af
Tom Pereira <tompe@asos.com>
2016-03-17 00:01:05 +0000

    Test google map with custom locator

---
+++ Asos.FashionEmergency.Web/Controllers/MapController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Asos.FashionEmergency.Web.Controllers
+{
+    public class MapController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asos.FashionEmergency.Web/scripts/map.js
@@ -0,0 +1,18 @@
+﻿var map, center;
+function initMap() {
+    google.maps.event.addDomListener(window, 'load', function() {
+        var marker = new RichMarker({
+            position: center,
+            map: map,
+            draggable: true,
+            content: '<img src="../images/asos-locator.png" class="locator">'
+        });
+        marker.setShadow('');
+    });
+
+    center = new google.maps.LatLng(51.5233512, -0.08101320000002943);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 12
+    });
+}
