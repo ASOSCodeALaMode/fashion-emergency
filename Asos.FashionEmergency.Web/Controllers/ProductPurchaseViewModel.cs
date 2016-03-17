8d4b0b6aee10667455701539907a74ca00e50b24
timurasos <timur.hasan@asos.com>
2016-03-17 04:03:53 +0000

    It's cool

--- Asos.FashionEmergency.Web/Controllers/ProductPurchaseViewModel.cs
+++ Asos.FashionEmergency.Web/Controllers/ProductPurchaseViewModel.cs
@@ -5,16 +5,25 @@ namespace Asos.FashionEmergency.Web.Controllers
 {
     public class ProductPurchaseViewModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Post Code")]
         public string PostCode { get; set; }
-        public string CreditCardNumber { get; set; }
+
         public int ProductId { get; set; }
+
         public TimeSlotData TimeSlotInfo { get; set; }
 
         [Display(Name = "Delivery Time Slot")]
         public string SelectedTimeSlotId { get; set; }
+
         public decimal DeliveryPrice { get; set; }
+
         public decimal ProductPrice { get; set; }
     }
 }
